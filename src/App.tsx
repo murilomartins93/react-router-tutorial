e519cc9e6266836610c394fa3a8562cd17492d89
Murilo Martins <murilomartins@outlook.com>
2023-02-23 15:58:24 -0300

    :books: Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <h1>Bookkeeper!</h1>;
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
