92ae30c20c4f8475927397e2a0ea0c69f8494f73
Murilo Martins <murilomartins@outlook.com>
2023-02-23 17:44:24 -0300

    :books: Index Routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,14 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="*" element={<NotFound />} />
