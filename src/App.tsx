50a2219ee3b7a9efb9df81cce0f773e9a749d86d
Murilo Martins <murilomartins@outlook.com>
2023-02-23 16:36:55 -0300

    :books: Nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
     </div>
   );
 }
